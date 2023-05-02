eb7f456e602ee40024be393d11cb07ef45103d87
Antonio <antonioboksic1603@gmail.com>
2023-05-02 17:36:04 +0200

    ora riesco a printare contenuti correttmente

--- js/main.js
+++ js/main.js
@@ -2,35 +2,30 @@
 // const container = document.getElementById("container");
 
 // console.log(container)
-
+let text;
 
 
 for (let i=1; i<=100; i++) {
 
-    console.log(i)
-    
-    let quadrato
-    
-   
-    
+    // console.log(i)
+
     
 
     if (i % 3 == 0 && i % 5 == 0) {
-        quadrato = `<span class="square">"fizzbuzz"</span>`
+        text = "fizzbuzz"
         // quadrato.classList.add("fizzbuzz");
     } else if (i % 5 == 0)  {
-        quadrato = `<span class="square">buzz</span>`
+        text = "buzz"
     } else if (i % 3 == 0) {
-        quadrato = `<span class="square">fizz</span>`
+        text = "fizz"
     } 
     else {
-        quadrato = `<span class="square">${i}</span>`
+        text = i
     }
 
+    console.log(text)
 
-    
-
-    container.innerHTML += quadrato;
+    // container.innerHTML += quadrato;
 
  }
  
